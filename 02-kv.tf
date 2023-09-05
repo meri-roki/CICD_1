f51100a571e2c2d6f63df3ab96c11262a8e6015a
root <root@meha.localdomain>
2023-09-05 10:25:29 +0200

    secounf commit

--- 02-kv.tf
+++ 02-kv.tf
@@ -2,6 +2,7 @@
 #  key_vault_id = data.azurerm_key_vault.keyvault_iac.id
 #}
 #
+
 #data "azurerm_key_vault_secret" "global_iac" {
 #  for_each     = toset(data.azurerm_key_vault_secrets.global_iac.names)
 #  name         = each.key
